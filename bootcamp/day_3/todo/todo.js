aeac25561c31b908bd18128a33605d4dde290fc4
Braulio Cald <calderon.braulio@yahoo.com>
2020-01-06 10:39:06 -0500

    Arrays

---
+++ bootcamp/day_3/todo/todo.js
@@ -0,0 +1,14 @@
+const notes = ['note 1', 'note 2', 'note 3']
+console.log(notes.length);
+console.log(notes[notes.length - 1]);
+
+
+// Challenge
+const todo = ['homework', 'make sure I finish this today', 'eat something']
+console.log(`I have following things to do ${todo.length}`);
+console.log(`The first one ${todo[0]}`);
+console.log(`The second one ${todo[1]}`);
+console.log(`The third one ${todo[2]}`);
+
+
+
